127827dc3033f1ef7e6e5b00cf25855f263102f8
ItzCherokee <152844615+ItzCherokee@users.noreply.github.com>
2024-12-14 18:06:55 +0330

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -10,12 +10,12 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">آنلاین!${data.players.online} Online</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;سرور آنلاین است!&ensp;&middot;&ensp;${data.players.online} پلیر در حال بازی هستن!</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;سرور آنلاین است&ensp;&middot;&ensp;${data.players.online} پلیر در حال بازی هستن</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;آنلاین, ${data.players.online} پلیر</p>`;
         {% endif %}
     }
     else {
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">فعلا سرور آفلاین است!</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">فعلا سرور آفلاین است</span>`;
         document.getElementById("serverIP").classList.remove("bg-green-900");
         document.getElementById("serverIP").classList.add("bg-orange-800");
 
@@ -23,8 +23,8 @@
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;متاسفانه سرور آفلاین است!</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;متاسفانه سرور آفلاین است</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;آفلاین</p>`;
         {% endif %}
     }
 });
